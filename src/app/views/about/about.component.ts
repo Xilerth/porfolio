cd47f5767b74f910d121fc9ecce9a47e98546235
Xilerth <xilerth@gmail.com>
2023-02-27 02:11:17 +0100

    page

--- src/app/views/about/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'xilerth-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
-})
-export class AboutComponent {}
